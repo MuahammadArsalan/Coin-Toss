4a631b108caf2ae59abd346f3eed564b450192db
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-04-24 09:32:04 +0530

    Coin toss

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-// round (if the decimal  is equal to or greater  than .5 the next number will be consider)
-// ceil  (not effected the decimal the next number will be consider)
-// floor (not effected the decimal the prevoius number will be consider)
-// parse Int
-// parse float
-
-
-
-// let num = 30.5;
-// console.log(Math.round(num));
-
-// console.log(Math.ceil(num));
-
-// console.log(Math.floor(num));
-
-
-
-
-// let numb = '20.22222';
-// let numb1 = 20.22222;
-
-// parseInt (not effected the decimal the prevoius number will be consider)
-
-// console.log(typeof(parseInt(numb)));
-// console.log(typeof(parseFloat(numb)));
-// console.log(parseInt(numb));
-
-// console.log(parseInt(numb));
-
-// console.log(parseInt(numb1));
-
-// console.log(parseFloat(numb));
-
-// console.log(parseFloat(numb1));
-
 
 // Toss Task Started
 
@@ -50,61 +15,21 @@ function toss(clickBtn){
 
 if(tossVal===0){
 
-div.innerHTML=`<p><img src="./Chaand.jpeg" alt="chaand-image" width='200px' height="200px"></p> <br> `;
+
+div.innerHTML =`<p><img src="./images/Chaand.jpeg" width="200px" height="200px" alt=""></p>`
 
 }else if(tossVal===1){
-div.innerHTML=`<p><img src="./masjid.jpg" alt="masjid-image" width='200px' height="200px"></p> <br> `
+
+div.innerHTML = `<p><img src="./images/masjid.jpg" width="200px" height="200px" alt=""></p>`
 
 } if(clickBtn === 'Chaand' && tossVal=== 0 || clickBtn ==='Masjid' && tossVal === 1){
     paragraph.innerHTML="You win the toss";
-    // console.log(`You win the toss`);
 
 }else{
    paragraph.innerHTML= `You lose the toss`;
 
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Chaand(){
-//     if(div.innerHTML===`<p><img src="./masjid.jpg" alt="masjid-image" width='100px' height="100px"></p>`){
-//         div.innerHTML=`you win the toss`;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
